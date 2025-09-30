58ea1588467391efec38c51d8141ef251ccaef48
jcoon3 <jcoon3@fingerlakes.edu>
2025-09-30 19:01:23 +0000

    coin_toss.py take 1

--- coin_toss.py
+++ coin_toss.py
@@ -1,67 +1,16 @@
-# FILE NAME - coin_toss.py
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
-# 1. Make sure you fill out the comments above
-# 2. Write your code in the proper spot
-# 3. Be sure to answer the Reflection Questions and Attestation below
-# 4. The Sample Output has been included in this code for your convenience
-########## ENTER YER CODE BELOW THIS LINE ##########
+# Name: Justin Coon
+# Date: 09-29-2025
+# Description: A simple coin flipper program
 
+import random
 
+print("===== Coin Flipper =====")
 
+coin_flip = random.randint(1, 100)
 
-
-
-
-
-
-
-########### END YER CODE ABOVE THIS LINE ###########
-
-
-########################################
-#          SAMPLE OUTPUT
-########################################
-'''
-===== Coin Flipper =====
-Heads
-'''
-
-
-
-'''
-===== Coin Flipper =====
-Tails
-'''
-
-
-########################################
-#          REFLECTION QUESTIONS
-########################################
-
-'''
-
-1. What was the hardest part of completing this lab? 
-
-
-
-
-
-
-
-'''
-
-########################################
-#            ATTESTATION
-########################################
-'''
-It is critical in this class that you understand the concepts as we explore them because
-those concepts are required understanding for entry level programming. Reliance on resources
-like AI and internet sites like Chegg, CourseHero, StackOverflow, and general Google results
-may impede your understanding. Please rate how well you understand the concepts in this lab: 
-[ ] I understand very little about this lab.
-[ ] I am about 50/50 on this lab; I get parts of it but not the whole picture.
-[ ] I pretty much get it.
-[ ] I'm solid. Totally got it.
-'''
+if coin_flip >= 51:
+    print("Tails")
+    
+else:
+    print("Heads")
+    
